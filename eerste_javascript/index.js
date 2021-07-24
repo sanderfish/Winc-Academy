5b32b52d68a236370326aed06304c20117a4338b
Puck Visser <86365607+puckvis@users.noreply.github.com>
2021-07-24 11:15:01 +0200

    Add files via upload

---
+++ eerste_javascript/index.js
@@ -0,0 +1,27 @@
+console.log("Hello Winc Academy");
+
+// eigen naam
+let name = 'Puck';
+console.log(name);
+
+// naam 'if'
+name = 'if';
+console.log(name);
+
+// strings optellen lukt niet
+let stringNumber = '4';
+console.log(stringNumber + stringNumber);
+
+// numbers optellen
+let firstNumber = 4;
+let secondNumber = 8;
+let thirdNumber = 40;
+console.log(firstNumber + firstNumber);
+console.log(secondNumber - firstNumber);
+console.log(firstNumber * firstNumber);
+console.log(secondNumber / firstNumber);
+console.log(firstNumber % thirdNumber);
+
+// leeftijd
+let age = '31';
+console.log(age);
