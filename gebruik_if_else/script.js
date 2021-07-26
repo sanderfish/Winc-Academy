aac58a91c1c7c1578a6e6342e6866192d89e4519
Puck Visser <86365607+puckvis@users.noreply.github.com>
2021-07-26 16:03:57 +0200

    Add files via upload

---
+++ gebruik_if_else/script.js
@@ -0,0 +1,24 @@
+const age = 18;
+const isFemale = false;
+const driverStatus = 'bob';
+
+if (age >= 18) {
+    console.log("Yes, je bent 18 of ouder!");
+
+} else if (age < 18) {
+    console.log("Helaas, nog eventjes wachten tot je 18 bent! Volgende keer beter :-)")
+}
+
+if (isFemale) {
+console.log("Welkom op LadiesNight!");
+
+} else {
+    console.log("Helaas, je bent geen vrouw.");
+}
+
+if (driverStatus) {
+    console.log("Je bent de bob en mag dus rijden!");
+
+} else {
+    console.log("Helaas, je bent geen bob en mag niet rijden.");
+}
