8cbf89da30b136231222924acf854801a3235a63
Puck Visser <86365607+puckvis@users.noreply.github.com>
2021-07-28 14:40:37 +0200

    Add files via upload

--- function_basics/script.js
+++ function_basics/script.js
@@ -2,25 +2,25 @@
 console.log('Maak deeg'); */
 
 // functie pizza maken
-function maakPizza() {
+const maakPizza = function () {
     console.log('Maak deeg en rol er een bal van');
     console.log('Rol de deegbal uit tot een mooie pizzabodem');
     console.log('Maak tomatensaus en rasp de kaas');
     console.log('Smeer de tomatensaus en geraspte kaas op de pizzabodem');
     console.log('Doe de belegde pizza in de steenoven');
-}
+};
 
 /* maakPizza(); */
 
 // functie sushi maken
 
-function maakSushi() {
+const maakSushi = function () {
     console.log('Kook de sushirijst');
     console.log('Voeg rijstazijn toe');
     console.log('Smeer de rijst uit op het sushimatje');
     console.log('Leg daar ingrediënten naar wens op');
     console.log('Oprollen, snijden en eten maar!');
-}
+};
 
 /* maakSushi(); */
 
