7dcf1894410632e3db47fb320fe5b5b7f5535315
Puck Visser <86365607+puckvis@users.noreply.github.com>
2021-08-02 14:34:47 +0200

    Update multiplication-tables.js

--- while_loops_for_loops/multiplication-tables.js
+++ while_loops_for_loops/multiplication-tables.js
@@ -1,4 +1,4 @@
-/* // table of 9
+// table of 9
 const multiplier = 9;
 for (let i = 1; i <= 10; i++) {
     let result = i * multiplier;
@@ -10,13 +10,13 @@ for (let i = 1; i <= 10; i++) {
 // 2 x 9 = 18 
 // 3 x 9 = 27 
 // etc...
-
-*/
+/*
 
 // tables 1 until 10
-for (let multiplier = 1; multiplier <= 1-; multiplier++) {
+for (let multiplier = 1; multiplier <= 10; multiplier++) {
     for (let i = 1; i <= 10; i++) {
         let result = i * multiplier;
         console.log(i + " x " + multiplier + " = " + result);
     }
 }
+*/
